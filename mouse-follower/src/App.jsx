cb46eea075ef0cb8ea2fdb03ab581597c702a9aa
AIGARZABAL <aqui.igarzabal@gmail.com>
2024-03-03 12:04:28 -0300

    primer commit

---
+++ mouse-follower/src/App.jsx
@@ -0,0 +1,59 @@
+import { useEffect, useState } from "react"
+
+function App() {
+
+  const [enabled, setEnabled] = useState(false)
+  const [position, setPosition] = useState({ x: 0, y: 0 })
+
+  useEffect(() => {
+    document.body.classList.toggle('no-cursor', enabled)
+
+    return () => {
+      document.body.classList.remove('no-cursor')
+    }
+  }, [enabled])
+
+  useEffect(() => {
+    const handleMove = (event) => {
+      const { clientX, clientY } = event
+      setPosition({ x: clientX, y: clientY })
+    }
+
+    if (enabled) {
+      window.addEventListener('pointermove', handleMove)
+    }
+
+    return () => { // cleanup method
+      window.removeEventListener('pointermove', handleMove)
+    }    
+  },[enabled])
+
+  const handleClick = () => {
+    setEnabled(!enabled)
+  }
+  
+  return(
+    <main>
+      <h1>Proyecto React</h1>
+      <div style={{
+        position: 'absolute',
+        backgroundColor: 'rgba(0, 0, 0, 0.5)',
+        border: '1px solid #fff',
+        borderRadius: '50%',
+        opacity: 0.8,
+        pointerEvents: 'none',
+        left: -25,
+        top: -25,
+        width: 50,
+        height: 50,
+        transform: `translate(${position.x}px, ${position.y}px)`
+      }}
+      />
+      <button onClick={handleClick}>
+        {enabled? "Desactivar seguir puntero" : "Activar seguir puntero"}
+      </button>
+    </main>
+  )
+}
+
+export default App
